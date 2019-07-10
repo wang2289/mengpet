d3bf16730a4f7ad3716a9981ee15892f38eaa13c
wangjingwen <18852982289@163.com>
2019-07-10 15:06:26 +0800

    页面2

---
+++ pages/sendinfor/sendinfor.js
@@ -0,0 +1,59 @@
+// pages/book/book.js
+import { BookModel } from '../../models/book.js'
+import { random } from '../../utils/util.js'
+let bookModel = new BookModel()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    searchPanel: false,
+    books: Object,
+    items: [
+      { name: '0', value: '弟弟' },
+      { name: '1', value: '妹妹', checked: 'true' },
+    ],
+    items2: [
+      { name: '0', value: '60后', checked: 'true' },
+      { name: '1', value: '70后' },
+      { name: '2', value: '80后' },
+      { name: '3', value: '90后' },
+      { name: '4', value: '00后' },
+    ],
+    more: false
+  },
+
+  onReachBottom: function (event) {
+    this.setData({
+      more: random(16)
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    bookModel.getHotList((data) => {
+      this.setData({
+        books: data
+      })
+    })
+  },
+
+  onActivateSearch: function (event) {
+    this.setData({
+      searchPanel: true
+    })
+  },
+
+  onCancel: function (event) {
+    this.setData({
+      searchPanel: false
+    })
+  },
+
+  onShareAppMessage() {
+
+  }
+})
