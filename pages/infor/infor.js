6d77170fbf470c51e5a75c6ad0894f3cd80e54a5
wangjingwen <18852982289@163.com>
2019-10-15 14:29:54 +0800

    我的资料页

--- pages/infor/infor.js
+++ pages/infor/infor.js
@@ -10,26 +10,94 @@ Page({
   data: {
     searchPanel:false,
     books:Object,
-    items: [
-      { name: '0', value: '先生' },
-      { name: '1', value: '女士', checked: 'true' },
-    ],
-    items2: [
-      { name: '0', value: '60后', checked: 'true' },
-      { name: '1', value: '70后' },
-      { name: '2', value: '80后' },
-      { name: '3', value: '90后' },
-      { name: '4', value: '00后' },
-    ],
-    more:false
+    radio1:"1",
+    radio2: true,
+    radio3: true,
+    radio4: "1",
+    radio5: true,
+    radio6: true,
+    more:false,
+    name:'',
+    weixing: '',
+    shouji: '',
+    diqu: '',
   },
-
+  onChangename(event) {
+    this.setData({
+      name: event.detail
+    });
+  },
+  onChangewei(event) {
+    this.setData({
+      weixing: event.detail
+    });
+  },
+  onChangeshou(event) {
+    this.setData({
+      shouji: event.detail
+    });
+  },
+  onChangedi(event) {
+    this.setData({
+      diqu: event.detail
+    });
+  },
+  onChangezhi(event) {
+    this.setData({
+      zhiye: event.detail
+    });
+  },
   onReachBottom: function (event) {
     this.setData({
       more:random(16)
     })
   },
-
+  onChange2: function(event) {
+    this.setData({
+      radio2: event.detail
+    });
+  },
+  onChange3: function (event) {
+    this.setData({
+      radio3: event.detail
+    });
+  },
+  onChange5: function (event) {
+    this.setData({
+      radio5: event.detail
+    });
+  },
+  onChange6: function (event) {
+    this.setData({
+      radio6: event.detail
+    });
+  },
+  onChange4: function (event) {
+    this.setData({
+      radio4: event.detail
+    });
+  },
+  onChange1: function (event) {
+    this.setData({
+      radio1: event.detail
+    });
+  },
+  onSave: function () {
+    var parms = {
+      name: this.data.name,
+      sex: this.data.radio1,
+      weixing: this.data.weixing,
+      shouji: this.data.shouji,
+      diqu: this.data.diqu,
+      zhiye: this.data.zhiye,
+      weigong: this.data.radio2,
+      phonegong: this.data.radio3,
+      ages: this.data.radio4,
+      diqugong: this.data.radio5,
+      zhiyegong: this.data.radio6,
+    };
+    console.log(parms);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
