799f00fac097e7eff86a101bee3d1ac6b7463504
spicis <8928565@qq.com>
2019-10-24 19:24:50 +0800

    修改图片服务器地址

--- utils/config.js
+++ utils/config.js
@@ -5,9 +5,10 @@ class Config {
 }
 
 Config.apiUrl = 'http://47.94.161.221:8092/zmengstar_miniprogram';
-//Config.apiUrl = 'http://localhost:8091/zmengstarwx';
+// Config.apiUrl = 'http://localhost:8091/zmengstarwx';
 Config.tokenUrl ='https://identityservercenter.kjcytk.com';
-Config.imgPath = '/images/uploadImages/image';
+// Config.imgPath = '/images/uploadImages/image';
+Config.imgPath = 'http://47.94.161.221/images/uploadImages/image';
 Config.classid=5000;
 Config.client_id= 'weixin';
  Config.client_secret='secret';
